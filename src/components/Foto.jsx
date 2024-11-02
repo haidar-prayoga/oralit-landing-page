e4bafb2029c0bdf5de1ed6887029d2c2b8eb696f
Haidar <haidarprayoga9b@gmail.com>
2024-11-02 12:34:05 +0700

    change route img

--- src/components/Foto.jsx
+++ src/components/Foto.jsx
@@ -1,7 +1,7 @@
 const Foto = () => {
     return (
         <img
-            src="../public/assets/obat.png"
+            src="/public/assets/obat.png"
             alt="obat"
             className="lg:w-[100%] w-[300px] mx-auto lg:float-left lg:left-1 rounded-md bg-white-shadow md:clear-left m-5"
         />
